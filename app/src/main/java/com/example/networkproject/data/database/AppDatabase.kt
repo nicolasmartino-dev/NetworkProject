bcb9c016c8ae9c06824c2836d8ad9ecaf477c6f5
Nicolas Martino <nmartino@Mac-mini-de-Nicolas-Martino.local>
2025-09-25 13:23:35 -0400

    update gradle configuration

--- app/src/main/java/com/example/networkproject/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/networkproject/data/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.networkproject.data.database.dao.EmployeeDao
 import com.example.networkproject.data.database.entities.Employee
 
-@Database(entities = [Employee::class], version = 1)
+@Database(entities = [Employee::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun employeeDao(): EmployeeDao
 }
